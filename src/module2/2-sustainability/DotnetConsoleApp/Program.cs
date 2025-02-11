0fb587badb0a3172aee78ee681ea970d7a87d1bb
james.eastham <james.eastham@datadoghq.com>
2025-02-11 12:42:09 +0000

    Add initial code samples

---
+++ src/module2/2-sustainability/DotnetConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+Console.WriteLine("Starting GC stress test...");
+Console.WriteLine($"Gen 0 collections: {GC.CollectionCount(0)}");
+Console.WriteLine($"Gen 1 collections: {GC.CollectionCount(1)}");
+Console.WriteLine($"Gen 2 collections: {GC.CollectionCount(2)}");
+
+// Run multiple tasks to create pressure
+var tasks = new List<Task>();
+for (int i = 0; i < Environment.ProcessorCount; i++)
+{
+    tasks.Add(Task.Run(AllocateMemory));
+}
+
+// Every second, print GC statistics
+while (!Console.KeyAvailable)
+{
+    await Task.Delay(1000);
+    Console.WriteLine("\nGC Collections:");
+    Console.WriteLine($"Gen 0: {GC.CollectionCount(0)}");
+    Console.WriteLine($"Gen 1: {GC.CollectionCount(1)}");
+    Console.WriteLine($"Gen 2: {GC.CollectionCount(2)}");
+    Console.WriteLine($"Total Memory: {GC.GetTotalMemory(false) / 1024 / 1024} MB");
+}
+
+static void AllocateMemory()
+{
+    var random = new Random();
+    while (true)
+    {
+        // Create large byte arrays
+        var largeArray = new byte[random.Next(1024 * 1024, 5 * 1024 * 1024)];
+
+        // Create many small objects
+        var objects = new List<object>();
+        for (int i = 0; i < 1000; i++)
+        {
+            objects.Add(new string('x', random.Next(100, 1000)));
+        }
+
+        // Force some Gen 2 objects to be created
+        if (random.Next(100) < 5)
+        {
+            GC.Collect(2, GCCollectionMode.Forced);
+        }
+    }
+}
---
+++ src/module3/DotnetDataTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+var stringExample = "Hello";
+var integerExample = 10;
+var floatExample = 10.5f;
+var arrayExample = new string[1] {"Hello"};
+var listExample = new List<string>();
+var boolExample = true;
+
+var user = new User("James");
+user.SayHello();
+
+user.UpdateName("John");
+
+user.SayHello();
+
+var premiumUser = user.UpgradeToPremium();
+
+user.SayHello();
+premiumUser.SayHello();
+
+class User {
+    public string Name { get; set; }
+
+    public User(string name) {
+        Name = name;
+    }
+
+    public void UpdateName(string newName) {
+        Name = newName;
+    }
+
+    public virtual void SayHello() {
+        Console.WriteLine("Hello! I'm " + Name + ", I'm a standard user");
+    }
+
+    public PremiumUser UpgradeToPremium() {
+        return new PremiumUser(this.Name);
+    }
+}
+
+class PremiumUser : User {
+    public PremiumUser(string name): base(name) {
+
+    }
+
+    public override void SayHello() {
+        Console.WriteLine("Hello! I'm " + Name + ", I'm a premium user");
+    }
+
+    private bool isPremium = true;
+}
